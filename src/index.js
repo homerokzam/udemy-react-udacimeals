b7d7073eab0b88a7e0aa30525257b5f0d9812626
Homero Batista Kzam <homero@parkcode.mobi>
2018-09-19 14:56:49 -0300

    Init

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+
+import './index.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import reducer from './reducers';
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+console.log(store.getState());
+
+ReactDOM.render(
+  <Provider store={store} >
+    <App />
+  </Provider>, document.getElementById('root'));
+registerServiceWorker();
